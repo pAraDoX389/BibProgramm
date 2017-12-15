8cdf4582518da227c6bb73312173283c17d1cd5a
Andre Grüneberg <Gruenie89@web.de>
2017-12-15 19:49:15 +0100

    Start des Projekts

---
+++ BibProgramm/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: ansible
+ *
+ * Created on 15. Dezember 2017, 19:29
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*hier soll das BibProgramm entstehen
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hier entsteht bald mehr!"; 
+    return 0;
+}
+
